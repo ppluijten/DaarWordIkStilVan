bc618350c54d82c86de09abe4dac473d9849defc
Pieter-Paul Luijten <ppluijten@gmail.com>
2016-05-19 22:06:14 +0200

    Basic solution for Visual Studio Typescript

---
+++ DaarWordIkStilVan/app.ts
@@ -0,0 +1,28 @@
+﻿class Greeter {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+        this.element.innerHTML += "De tijd is: ";
+        this.span = document.createElement("span");
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toLocaleString("nl-NL");
+    }
+
+    start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toLocaleString("nl-NL"), 500);
+    }
+
+    stop() {
+        clearTimeout(this.timerToken);
+    }
+
+}
+
+window.onload = () => {
+    var el = document.getElementById("content");
+    var greeter = new Greeter(el);
+    greeter.start();
+};
