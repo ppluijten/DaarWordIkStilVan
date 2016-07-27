8daaacb1a9c0d412d495329879443984963718ff
Pieter-Paul Luijten <ppluijten@gmail.com>
2016-07-27 22:05:03 +0200

    Adding activity overview rendering.

--- DaarWordIkStilVan/app.js
+++ DaarWordIkStilVan/app.js
@@ -5,6 +5,10 @@
             .each(function () {
             pages.render($(this));
         });
+        $(".activities")
+            .each(function () {
+            pages.renderActivities($(this));
+        });
     });
     var Pages = (function () {
         function Pages(folder) {
@@ -13,6 +17,20 @@
         Pages.prototype.render = function (element) {
             $.get(this.folder + "/" + element.data("page") + ".html", function (data) {
                 element.html(data);
+            });
+        };
+        Pages.prototype.renderActivities = function (element) {
+            var _this = this;
+            element.html("");
+            $.get(this.folder + "/activiteiten.json", function (data) {
+                for (var i = 0; i < data.activities.length; i++) {
+                    _this.renderAcvitity(element, data.activities[i].name, data.activities[i].title);
+                }
+            });
+        };
+        Pages.prototype.renderAcvitity = function (element, activityname, activitytitle) {
+            $.get(this.folder + ("/activiteiten/descriptions/" + activityname + ".html"), function (data) {
+                element.append("<div class='row activity'>\n                    <div class='activity-image' style='background-image: url(pages/activiteiten/images/" + activityname + ".png);'></div>\n                    <div class='activity-description'>\n                        <h1>" + activitytitle + "</h1>\n                        <p>\n                            " + data + "\n                            <a href='#'>lees meer</a>\n                        </p>\n                    </div>\n                </div>");
             });
         };
         return Pages;
--- DaarWordIkStilVan/app.ts
+++ DaarWordIkStilVan/app.ts
@@ -2,11 +2,14 @@
 
 (($ => {
     $(() => {
-        // document.ready
         var pages = new Pages("pages");
         $("div[data-page]")
             .each(function () {
                 pages.render($(this));
+            });
+        $(".activities")
+            .each(function () {
+                pages.renderActivities($(this));
             });
     });
 
@@ -20,6 +23,30 @@
         render(element: JQuery) {
             $.get(this.folder + "/" + element.data("page") + ".html", data => {
                 element.html(data);
+            });
+        }
+
+        renderActivities(element: JQuery) {
+            element.html("");
+            $.get(this.folder + "/activiteiten.json", data => {
+                for (var i = 0; i < data.activities.length; i++) {
+                    this.renderAcvitity(element, data.activities[i].name, data.activities[i].title);
+                }
+            });
+        }
+
+        renderAcvitity(element: JQuery, activityname: string, activitytitle: string) {
+            $.get(this.folder + `/activiteiten/descriptions/${activityname}.html`, data => {
+                element.append(`<div class='row activity'>
+                    <div class='activity-image' style='background-image: url(pages/activiteiten/images/${activityname}.png);'></div>
+                    <div class='activity-description'>
+                        <h1>${activitytitle}</h1>
+                        <p>
+                            ${data}
+                            <a href='#'>lees meer</a>
+                        </p>
+                    </div>
+                </div>`);
             });
         }
     }
